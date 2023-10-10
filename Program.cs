d4b264d6c8d29a8e229ede4659fa0fef68f1a09d
Dual Tachyon <DualTachyon@users.noreply.github.com>
2023-10-10 15:16:21 +0100

    First implementation.

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO.Ports;
+using System.Runtime.CompilerServices;
+
+namespace RT_890_Flasher_CLI {
+	internal class Program {
+		static void Usage()
+		{
+			string[] Parts = Environment.CommandLine.Split(new char[] { System.IO.Path.DirectorySeparatorChar });
+			string Exe = Parts[Parts.Length - 1];
+			Exe = Exe.Trim();
+			Console.WriteLine("Usage:");
+			Console.WriteLine("\t" + Exe + " -l                        List available COM ports");
+			Console.WriteLine("\t" + Exe + " -p COMx -f firmware.bin   Flash a file");
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("RT-890-Flash-CLI (c) Copyright 2003 Dual Tachyon\n");
+			switch (args.Length) {
+			case 1:
+				if (args[0] != "-l") {
+					Usage();
+					break;
+				}
+				var Ports = SerialPort.GetPortNames();
+				Console.Write("Ports available:");
+				foreach (var Port in Ports) {
+					Console.Write(" " + Port);
+				}
+				Console.WriteLine();
+				break;
+			case 4:
+				if (args[0] != "-p") {
+					Usage();
+					break;
+				}
+				if (args[2] != "-f") {
+					Usage();
+					break;
+				}
+
+				byte[] Firmware;
+				try {
+					Firmware = System.IO.File.ReadAllBytes(args[3]);
+				} catch {
+					Console.WriteLine("Failed to read file!");
+					break;
+				}
+
+				RT_890_UART RT = new RT_890_UART();
+				try {
+					RT.Open(args[1]);
+				} catch {
+					Console.WriteLine("Failed to open COM port!");
+					break;
+				}
+
+				try {
+					if (!RT.IsBootLoaderMode()) {
+						Console.WriteLine("RT-890 is not in bootloader mode!");
+						break;
+					}
+				} catch {
+					Console.WriteLine("Timeout error! Is the radio in bootloader mode?");
+					break;
+				}
+				try {
+					if (!RT.Command_EraseFlash()) {
+						Console.WriteLine("Failed to erase flash!");
+						break;
+					}
+				} catch (Exception Ex) {
+					Console.WriteLine("\rUnexpected failure erasing flash! Error: ", Ex.Message);
+					break;
+				}
+
+				try {
+					ushort i;
+
+					for (i = 0; i < Firmware.Length; i += 128) {
+						Console.Write("\rFlashing at 0x" + i.ToString("X4"));
+						Console.Out.Flush();
+						if (!RT.Command_WriteFlash(i, Firmware)) {
+							Console.WriteLine("\rFailed to flash at 0x" + i.ToString("X4") + "!");
+							Console.Out.Flush();
+							break;
+						}
+					}
+					if (i == Firmware.Length) {
+						Console.WriteLine("\rFlashing complete!");
+					}
+				} catch (Exception Ex) {
+					Console.WriteLine("\rUnexpected failure writing to flash! Error: ", Ex.Message);
+				}
+				Console.WriteLine();
+				RT.Close();
+				break;
+
+			default:
+				Usage();
+				break;
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Support
+
+* If you like my work, you can support me through https://ko-fi.com/DualTachyon
+
+# Flash utility for the Radtel RT-890 radio
+
+I wrote this utility because there was no available flasher, official or otherwise, for this radio.
+It was written fairly quickly in C# in .NET and thus targets mainly Windows.
+It is known to work correctly under Linux with WINE, although the visual appearance may have some glitches.
+
+# Building
+
+Visual Studio 2022 Community Edition was used to build this utility.
+
+# Instructions
+
+Run the executable file to get (simple) instructions.
+
+# License
+
+Copyright 2023 Dual Tachyon
+https://github.com/DualTachyon
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
